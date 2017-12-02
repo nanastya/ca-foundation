61797b496fb217bef1f485665f0510685b333ebb
Nastya <anastasia.klimenko@beetroot.se>
2017-12-02 13:19:26 +0200

    animation

--- assets/src/javascript/scripts.js
+++ assets/src/javascript/scripts.js
@@ -2,9 +2,41 @@
 ;
 (function($){
 
+    function isScrolledIntoView(elem) {
+        if (elem.length) {
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+
+            var elemTop = $(elem).offset().top;
+            var elemBottom = elemTop + $(elem).height();
+
+            return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        }
+        return false;
+    }
+
     $(document).ready( function () {
         //Foundation init
         $(document).foundation();
+
     });
+    
+    $(document).on( 'scroll', function () {
+        const sectionTitle = $('.section-title');
+
+        sectionTitle.each( function () {
+            if ( isScrolledIntoView($(this)) ) {
+                $(this).addClass('section-title--animated');
+            }
+        });
+
+
+
+        const section = $('.title-subtitle');
+        if ( isScrolledIntoView( section ) ) {
+            section.addClass('title-subtitle--animated');
+        }
+
+    })
     
 })(jQuery);
